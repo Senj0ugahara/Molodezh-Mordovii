a7e248257b15af603fd5f4aecca222617914292d
Senj0ugahara <kamizu00@mail.ru>
2023-12-08 15:59:51 +0300

    initital

--- resources/libraries/inputMask.js
+++ resources/libraries/inputMask.js
@@ -1,24 +1,26 @@
 // Маска телефона
-let phoneInput = document.querySelector('.form__input[name="phone"]')
-phoneInput.addEventListener('keydown', function(e) {
-  if( !(e.key == 'ArrowLeft' || e.key == 'ArrowRight' || e.key == 'Backspace' || e.key == 'Tab')) { event.preventDefault() }
-    let mask = '+7 (111) 111-11-11';
-
-    if (/[0-9\+\ \-\(\)]/.test(e.key)) {
-        let currentString = this.value;
-        let currentLength = currentString.length;
-        if (/[0-9]/.test(e.key)) {
-            if (mask[currentLength] == '1') {
-                this.value = currentString + e.key;
-            } else {
-                for (let i=currentLength; i<mask.length; i++) {
-                if (mask[i] == '1') {
+if (document.querySelector('.form-main')) {
+    let phoneInput = document.querySelector('.form__input[name="phone"]')
+    phoneInput.addEventListener('keydown', function(e) {
+      if( !(e.key == 'ArrowLeft' || e.key == 'ArrowRight' || e.key == 'Backspace' || e.key == 'Tab')) { event.preventDefault() }
+        let mask = '+7 (111) 111-11-11';
+    
+        if (/[0-9\+\ \-\(\)]/.test(e.key)) {
+            let currentString = this.value;
+            let currentLength = currentString.length;
+            if (/[0-9]/.test(e.key)) {
+                if (mask[currentLength] == '1') {
                     this.value = currentString + e.key;
-                    break;
-                }
-                currentString += mask[i];
+                } else {
+                    for (let i=currentLength; i<mask.length; i++) {
+                    if (mask[i] == '1') {
+                        this.value = currentString + e.key;
+                        break;
+                    }
+                    currentString += mask[i];
+                    }
                 }
             }
         }
-    }
-});
+    });
+}
